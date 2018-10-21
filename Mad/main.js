a52cade113e99814cb31b32cf802fc0f88f99d91
manson162 <kevin.torrington@gmail.com>
2018-10-21 11:16:37 +0100

    Maddy had a play

---
+++ Mad/main.js
@@ -0,0 +1,16 @@
+function buttonClick() {
+
+document.body.style.backgroundColor = "purple";
+
+}
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
+
